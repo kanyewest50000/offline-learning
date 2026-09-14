9be50c851bb89bfafe7a618374032d78ed375a95
kanyewest50000 <j3831174@gmail.com>
2026-09-14 11:51:54 -0700

    Update config.js

--- assets/js/shrine/config.js
+++ assets/js/shrine/config.js
@@ -28,7 +28,7 @@
      into about:blank, which has no base URL for relative paths to resolve against. */
   var TUNG_IMG = new URL("assets/tungtungtungsahur.png", location.href).href;
   var TUNGGOD_IMG = new URL("assets/tungtunggod.png", location.href).href;
-  var LENDER_IMG = new URL("assets/lendersahur1.webp", location.href).href;
+  var LENDER_IMG = new URL("assets/lendersahur.jpg", location.href).href;
 
   /* html-escape for anything interpolated into the shrine document */
   function esc(s) {
